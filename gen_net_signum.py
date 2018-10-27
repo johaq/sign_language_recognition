dc6008bcde37de41d9508d4ce63e85d228aa445e
Johannes Kummert <jkummert@techfak.uni-bielefeld.de>
2018-10-27 10:11:53 +0200

    stride for conv merge net

--- gen_net_signum.py
+++ gen_net_signum.py
@@ -168,7 +168,7 @@ class NetGen:
         feature_input = K.layers.Input(shape=(
             None, 390))
 
-        image_conv = K.layers.TimeDistributed(K.layers.Conv2D(filters=3, kernel_size=(10, 10)), name="bla")
+        image_conv = K.layers.TimeDistributed(K.layers.Conv2D(filters=3, kernel_size=(10, 10), strides=4))
         image_conv_out = image_conv(image_input)
 
         image_max = K.layers.TimeDistributed(K.layers.MaxPool2D(pool_size=(5, 5)))
